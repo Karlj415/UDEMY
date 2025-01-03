94994b1e2aa8df14aaa53a1cd583863108f90e12
Godly <karl.jung@yahoo.com>
2025-01-03 02:13:33 -0500

    Redid ALL Code
    Reformatted all html, cs, js to be more in line with instructors. Will be editing some CSS styles in the morning.
    
    TODO: Add hover, active, transform.

---
+++ UDEMY/UDEMY-WEBPAGE/shared.js
@@ -0,0 +1,39 @@
+let backdrop = document.querySelector(".backdrop");
+let modal = document.querySelector(".modal");
+let modalNoButton = document.querySelector(".modal__action--negative");
+let selectPlanButtons = document.querySelectorAll(".plan button");
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
+
+console.dir(backdrop);
+// console.dir(selectPlanButton);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add('open');
+    backdrop.classList.add('open');
+  });
+}
+
+backdrop.addEventListener("click", function() {
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
